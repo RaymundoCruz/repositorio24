a6f70578284e8faec8573c5b0b04e76e62bbeea5
raymundo cruz <Raymundo.CruzGarcia@gmail.com>
2024-04-27 12:56:12 -0600

    modifying menu

--- prueba.py
+++ prueba.py
@@ -1,6 +1,7 @@
 print("""Select an option from the menu:
       1.fibonacci using recursion
       2.fibonacci using iteration
-      3.fibonacci usin POO
-      4.
+      3.fibonacci using POO
+      4.fibonacci 
+      5.exit
       """)
